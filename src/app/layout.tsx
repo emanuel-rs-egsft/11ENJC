04c0e6c029f46b31c48c09de438d65b1e83ce39e
Emanuel João <emanuel.joao.rs@gmail.com>
2026-02-27 19:17:38 -0300

    fix: page.tsx como server

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -37,28 +37,20 @@ export const metadata: Metadata = {
     template: `%s | ${siteName}`,
   },
   description,
-  // ...
-  openGraph: {
-    type: "website",
-    url: "https://11-enjc.vercel.app/",
-    title: siteTitle,
-    description,
-    siteName,
-    locale: "pt_BR",
-    images: [
-      {
-        url: "/og/cover.png",
-        width: 1200,
-        height: 630,
-        alt: "11º ENJC - Capa",
-      },
+  icons: {
+    icon: [
+      { url: "/favicon.ico", sizes: "any" },
+      { url: "/icon-192.png", type: "image/png", sizes: "192x192" },
+      { url: "/icon-512.png", type: "image/png", sizes: "512x512" },
     ],
+    apple: [{ url: "/apple-touch-icon.png", sizes: "180x180" }],
   },
-  twitter: {
-    card: "summary_large_image",
+  openGraph: {
     title: siteTitle,
     description,
-    images: ["/og/cover.png"],
+    images: [{ url: "/og/cover.png", width: 1200, height: 630 }],
+    locale: "pt_BR",
+    type: "website",
   },
 };
 
