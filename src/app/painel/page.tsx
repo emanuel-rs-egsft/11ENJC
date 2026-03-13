0137b7ab99e396dcd8c5ed3d72c95530d391a3c2
Emanuel João <emanuel.joao.rs@gmail.com>
2026-03-13 15:01:22 -0300

    cria rota /painel para redirecionar para planilha

---
+++ src/app/painel/page.tsx
@@ -0,0 +1,12 @@
+export default function PainelPage() {
+  return (
+    <main style={{ width: "100%", height: "100vh" }}>
+      <iframe
+        src="https://docs.google.com/spreadsheets/d/1D6Pw6GtJtUtbGOPG0ZSLQaCez5RlohGEFWR8SlNKdWA/edit?gid=0#gid=0"
+        width="100%"
+        height="100%"
+        style={{ border: "none" }}
+      />
+    </main>
+  );
+}
